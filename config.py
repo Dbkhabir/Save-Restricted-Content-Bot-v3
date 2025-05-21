21d0b4db038ae6b0547e0d9f03a2cec655f464dd
Dbkhabir <123290961+Dbkhabir@users.noreply.github.com>
2025-05-21 23:24:14 +0600

    Update config.py

--- config.py
+++ config.py
@@ -17,11 +17,11 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = os.getenv("API_ID", "")
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-MONGO_DB = os.getenv("MONGO_DB", "")
-OWNER_ID = list(map(int, os.getenv("OWNER_ID", "").split())) # list seperated via space
+API_ID = os.getenv("API_ID", "28203009")
+API_HASH = os.getenv("API_HASH", "c385478ec9a0c322964bdd56175cef7b")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8192299885:AAHvUCydo213CnLETuMxWPl7PwLDhuZEy84")
+MONGO_DB = os.getenv("MONGO_DB", "mongodb+srv://modkha7110:modkha7110@cluster0.4jiwjny.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+OWNER_ID = list(map(int, os.getenv("OWNER_ID", "6401029823").split())) # list seperated via space
 DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
 STRING = os.getenv("STRING", None) # optional
 LOG_GROUP = int(os.getenv("LOG_GROUP", "-1001234456")) # optional with -100
